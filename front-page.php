799f40ef40e08843f36e5e2297449fd4dc0c96b9
Fabian295 <fabiankanhai70@gmail.com>
2023-02-09 03:05:59 +0100

    Updated

--- front-page.php
+++ front-page.php
@@ -1,11 +1,19 @@
-<?php get_header();?>
+<?php get_header('secondary');?>
+
+  <div class="container">
+
+
+    <h1><?php the_title();?></h1>
+
+    <?php get_template_part('includes/section', 'content');?>
+
+
+
+
+  </div>
 
 
 
 
 
-
-
-
-
-<?php get_footer();?>
+<?php get_footer('secondary');?>
--- header.php
+++ header.php
@@ -12,4 +12,8 @@
 
 </head>
 <body>
+
+<header>
+  
+</header>
   
