b08ee99bfd568527f0409939c63113726356f91d
Fabian295 <fabiankanhai70@gmail.com>
2023-02-09 03:09:37 +0100

    Created as extra template option / style or part

---
+++ footer-secondary.php
@@ -0,0 +1,29 @@
+
+
+<?php wp_footer();?>
+
+<div class="container">
+
+
+      
+    <div class="row pt-4 justify-content-between">
+
+      <div class="col-lg-4 col-md-2">
+        logo
+        <!-- <img src="/assets/fkcc.png" alt="logo" width="100%"> -->
+      </div>
+      <div class="col-lg-4 col-md-3">
+        <span>| FK &copy; 2023 | </span>
+      </div>
+      <div class="col-lg-4 col-md-2">
+        <div class="socials">Social Icons</div>
+      </div>
+
+    </div>
+
+
+
+</div>
+
+</body>
+</html>
